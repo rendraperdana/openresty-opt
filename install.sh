913485c3b325ee2e2c8256c738dbe3d07bfc5d96
root <rendraperdana@hotmail.com>
2020-03-02 12:32:04 +0700

    bundle

--- install.sh
+++ install.sh
@@ -1,58 +1,16 @@
 #!/bin/bash
 
-yum groupinstall 'Development Tools' -y
 yum install lua-devel readline-devel pcre-devel gcc perl-Digest-* zlib-devel -y
 
 SECONDS=0
 
-gmake clean
-
-cd openssl/fips/
-make clean
-./config no-ec2m no-ssl2 no-ssl3 no-weak-ssl-ciphers -O3 -Ofast -march=native
-make depend -j$(nproc)
-make -j$(nproc)
-
-cd ../
-make clean
-./config no-ec2m no-ssl2 no-ssl3 no-weak-ssl-ciphers -O3 -Ofast -march=native --prefix=/usr --openssldir=/usr shared
-make depend -j$(nproc)
-make -j$(nproc)
-
-cd ../
-
-./configure \
---with-cc-opt='-O3 -Ofast -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=native -DTCP_FASTOPEN=23' \
---with-pcre-jit \
---with-stream \
---with-stream_ssl_module \
---without-mail_pop3_module \
---without-mail_imap_module \
---without-mail_smtp_module \
---with-http_stub_status_module \
---with-http_realip_module \
---with-http_addition_module \
---with-http_auth_request_module \
---with-http_secure_link_module \
---with-http_random_index_module \
---with-http_gzip_static_module \
---with-http_sub_module \
---with-http_gunzip_module \
---with-threads \
---with-file-aio \
---with-http_ssl_module \
---with-openssl=openssl \
---with-openssl-opt='no-ec2m no-ssl2 no-ssl3 no-weak-ssl-ciphers -O3 -Ofast -w -m64 -mtune=native' \
--j$(nproc)
-
-gmake j$(nproc)
 gmake install
 rm -f /usr/lib/systemd/system/openresty.service
 cp ./util/openresty.service /usr/lib/systemd/system/ -v
 rm -f /usr/local/bin/openresty
 ln -s /usr/local/openresty/bin/openresty /usr/local/bin/openresty
-rm -rf /usr/local/openresty/conf/nginx.conf
-cp ./util/nginx.conf /usr/local/openresty/conf/nginx.conf
+rm -rf /usr/local/openresty/nginx/conf/nginx.conf
+cp ./util/nginx.conf /usr/local/openresty/nginx/conf/nginx.conf
 systemctl daemon-reload
 systemctl enable openresty
 
